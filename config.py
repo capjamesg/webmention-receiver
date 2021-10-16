a1756f0862d7d64206be8c5073871137fc15c9d2
capjamesg <jamesg@jamesg.blog>
2021-10-16 12:23:15 +0100

    fix bugs with auth

--- config.py
+++ config.py
@@ -17,8 +17,8 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 CALLBACK_URL = "https://webmention.jamesg.blog/callback"
 CLIENT_ID = "https://webmention.jamesg.blog/"
-TOKEN_ENDPOINT = "https://tokens.indieauth.com/token"
-AUTH_ENDPOINT = "https://indieauth.com/auth"
+TOKEN_ENDPOINT = "https://auth.jamesg.blog/token"
+AUTH_ENDPOINT = "https://auth.jamesg.blog/auth"
 ME = "https://jamesg.blog"
 
 # CALLBACK_URL = "http://localhost:5001/callback"
--- seed.py
+++ seed.py
@@ -8,26 +8,12 @@ import sqlite3
 
 seed_db = Blueprint('seed', __name__)
 
-@seed_db.cli.command("create-user")
-@click.argument("username")
-@click.argument("api_key")
-@click.argument("password")
-def create_user(username, api_key, password):
-    new_user = User(username=username, api_key=api_key, password=generate_password_hash(password, method='sha256'))
-
-    # add the new user to the database
-    db.session.add(new_user)
-    db.session.commit()
-    print("created user {}".format(username))
-
 @seed_db.cli.command("create-tables")
 def create_tables():
     connection = sqlite3.connect("webmentions.db")
 
     with connection:
         cursor = connection.cursor()
-
-        cursor.execute("CREATE TABLE user (id integer primary key autoincrement, api_key, publish_approved, registered_date);")
 
         cursor.execute("CREATE TABLE webmentions (id integer primary key autoincrement, source, target, property, contents, author_name, author_phopto, author_url, content_html, received_date, status);")
 
