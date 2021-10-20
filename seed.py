efc630896a8ca82cbd1068fb7c4107807449357f
capjamesg <jamesg@jamesg.blog>
2021-10-20 18:00:27 +0100

    fix webmention sending bug

--- seed.py
+++ seed.py
@@ -15,11 +15,11 @@ def create_tables():
     with connection:
         cursor = connection.cursor()
 
-        cursor.execute("CREATE TABLE webmentions (id integer primary key auto increment, source, target, property, contents, author_name, author_photo, author_url, content_html, received_date, status);")
+        cursor.execute("CREATE TABLE webmentions (id integer primary key autoincrement, source, target, property, contents, author_name, author_photo, author_url, content_html, received_date, status);")
 
-        cursor.execute("CREATE TABLE sent_webmentions (id integer primary key auto incement, source, target, sent_date, status_code, response, webmention_endpoint);")
+        cursor.execute("CREATE TABLE sent_webmentions (id integer primary key autoincement, source, target, sent_date, status_code, response, webmention_endpoint);")
 
-        cursor.execute("CREATE TABLE pending_webmentions (id integer primary key auto increment, to_check);")
+        cursor.execute("CREATE TABLE pending_webmentions (id integer primary key autoincrement, to_check);")
 
         cursor.execute("CREATE TABLE webhooks (feed_url text, last_url_sent text);")
 
